e8797ee25becaf4292b0c3d9979753a6318c8f1b
Carlos Mestre <carmesce@gmail.com>
2022-06-06 13:06:03 +0200

    Add Axios request

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello cervecerias");
+var axios = require('axios');
+
+var lat = '40.630710';
+var lon = '-3.165907';
+
+var config = {
+  method: 'get',
+  url: `https://api.openbrewerydb.org/breweries?by_dist=${lat},${lon}&per_page=1&page=0`,
+  headers: { }
+};
+
+axios(config)
+.then(function (response)
+{
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});

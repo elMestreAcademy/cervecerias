f8d96f7fe7760a8a2ea8e67af099c09471e7403b
Carlos Mestre <carmesce@gmail.com>
2022-06-06 13:35:31 +0200

    Listidado de Cervzas

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-var axios = require('axios');
-
-var lat = '40.630710';
-var lon = '-3.165907';
-
-var config = {
+const axios = require('axios');
+const express = require('express')
+const app = express()
+const port = 3000
+const lat = '40.630710';
+const lon = '-3.165907';
+const config = {
   method: 'get',
-  url: `https://api.openbrewerydb.org/breweries?by_dist=${lat},${lon}&per_page=1&page=0`,
+  url: `https://api.openbrewerydb.org/breweries?by_dist=${lat},${lon}&per_page=50&page=0`,
   headers: { }
 };
+var respuesta = {}
+
+function muestraBirrerira(birrerias) {
+
+    buffer = '<html><body><ol>';
+
+    for (birreria in birrerias)
+    {
+        buffer += `<li>${birrerias[birreria].name}</li>`;
+    }
+
+    buffer += '</ol></body></html>';
+    return buffer;
+}
 
 axios(config)
 .then(function (response)
 {
-  console.log(JSON.stringify(response.data));
+  respuesta = response.data;
 })
-.catch(function (error) {
+.catch(function (error)
+{
   console.log(error);
 });
+
+app.get('/', (req, res) =>
+{
+  miPage = muestraBirrerira(respuesta)
+  res.send(miPage);
+})
+
+app.listen(port, () =>
+{
+  console.log(`Example app listening on port ${port}`)
+})
